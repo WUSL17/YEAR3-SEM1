b5000e78e560d628083f3f6ee01d8b3a6d90891e
Chamod Shehanka <hcsperera@outlook.com>
2021-03-12 01:28:35 +0530

    Create Answers.R

---
+++ STAT 3232 - R/Tutorials/Tutorial 5/Answers.R
@@ -0,0 +1,14 @@
+# Question 1
+# No of trials = 18
+# No of success = 5
+binom.test(5,18)
+
+# Question 2
+prop.test(c(82,87),c(200,300))
+
+# Question 3
+samples<-c(103,216,171)
+probs<-c(0.18,0.5,0.32)
+chisq.test(samples,p=probs)
+
+# Question 4
